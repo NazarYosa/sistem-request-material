43328a2a2ae532d98b87e88b191d966571e2f3df
NazarYosa <nazaryosa@gmail.com>
2026-04-13 03:18:03 +0700

    fix: remove default machine label in generated labels

--- src/components/ManualReqView.jsx
+++ src/components/ManualReqView.jsx
@@ -108,7 +108,7 @@ const ManualReqView = ({
     else totalDisplay = `${totalPlan}`;
 
     generatedLabels.push({
-      machine: formData.machine ? formData.machine.toUpperCase() : "-",
+      machine: formData.machine ? formData.machine.toUpperCase() : "",
       partNameExcel: partData.partName || "-",
       partNoMain: partData.partNo || "-",
       model: partData.model || "-",
@@ -128,7 +128,7 @@ const ManualReqView = ({
 
   if (generatedLabels.length === 0) {
     generatedLabels.push({
-      machine: formData.machine ? formData.machine.toUpperCase() : "MESIN",
+      machine: formData.machine ? formData.machine.toUpperCase() : "",
       partNameExcel: partData.partName || "-",
       partNoMain: partData.partNo || "-",
       model: partData.model || "-",
